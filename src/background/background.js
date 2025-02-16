a8c19ff9c824901a20b6f2dce149818798d3dbb9
BDarbZ <robert.bradford.darby@gmail.com>
2025-02-15 21:26:55 -0600

    background.js is mainly working but will need tweaking later on

--- src/background/background.js
+++ src/background/background.js
@@ -3,9 +3,19 @@ let startTime = null;
 let elapsedTime = 0;
 let isTabActive = true;
 let timerInterval = null;
+let activeTabId = null;
+
+function sendMessageToTab(tabId, message) {
+  chrome.tabs.query({ active: true, windowId: chrome.windows.WINDOW_ID_CURRENT }, (tabs) => {
+      const tab = tabs[0];
+      if (tab && tab.id === tabId) {
+          chrome.tabs.sendMessage(tabId, message);
+      }
+  });
+}
 
 // Track tab switches
-chrome.tab.onActivated.addListener((activeInfo) => {
+chrome.tabs.onActivated.addListener((activeInfo) => {
     if(activeTabId !== activeInfo.tabId) {
         // Pause timer for previous tab
         pauseTimer();
@@ -14,6 +24,7 @@ chrome.tab.onActivated.addListener((activeInfo) => {
         activeTabId = activeInfo.tabId;
         startTime = Date.now();
         startTimer();
+        console.log('tab switch');
     }
 });
 
@@ -22,13 +33,15 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (tabId === activeTabId && changeInfo.status === 'complete') {
         startTime = Date.now();
         startTimer();
+        console.log('tab update');
     }
 });
 
 // Listen for window focus changes
 chrome.windows.onFocusChanged.addListener((windowId) => {
-    if (windowId === chrome.windows.WINDO_ID_NONE) {
+    if (windowId === chrome.windows.WINDOW_ID_NONE) {
         // Browser was minimized or unfocused
+        console.log('minimized');
         pauseTimer();
     } else {
         // Regain of focus
@@ -36,6 +49,7 @@ chrome.windows.onFocusChanged.addListener((windowId) => {
             if (tabs[0] && tabs[0].id === activeTabId) {
                 // Resume timer if same tab as before
                 startTimer();
+                console.log('refocussed');
             }
         });
     }
@@ -49,6 +63,7 @@ function startTimer() {
     timerInterval = setInterval(() => {
         if (activeTabId && startTime) {
             elapsedTime = (Date.now() - startTime) / 1000;
+            console.log(`Elapsed Time: ${elapsedTime} seconds`); // Log elapsed time
             checkSiteAndUpdateVegetation(activeTabId, elapsedTime);
         }
     }, 1000);
@@ -56,39 +71,40 @@ function startTimer() {
 
 // Pause timer function
 function pauseTimer() {
-    if (timerInterval) {
+  if (timerInterval) {
       clearInterval(timerInterval);
       timerInterval = null;
       isTabActive = false;
-      chrome.tabs.sendMessage(activeTabId, { action: 'pauseVegetation' });
-    }
+      if (activeTabId) {
+          sendMessageToTab(activeTabId, { action: "pauseVegetation" });
+          console.log('pause timer');
+
+      }
+  }
 }
 
 // Check if current site is in the website list
 function checkSiteAndUpdateVegetation(tabId, elapsedTime) {
-    chrome.storage.sync.get({ sites: [] }, (data) => {
-      const sites = data.sites; // Get the list of blocked sites
+  chrome.storage.sync.get({ sites: [] }, (data) => {
+      const sites = data.sites;
       chrome.tabs.get(tabId, (tab) => {
-        if (tab.url) {
-          const site = sites.find((s) => tab.url.includes(s.url)); // Check if the site is blocked
-          if (site) {
-            const totalTime = site.timer * 60; // Convert minutes to seconds
-            const remainingTime = totalTime - elapsedTime;
-            const percentage = Math.max(0, (elapsedTime / totalTime) * 100);
-  
-            if (remainingTime <= 0) {
-              // Lock the site if the timer runs out
-              chrome.tabs.sendMessage(tabId, { action: 'lockSite' });
-            } else if (remainingTime <= 5 * 60) {
-              // Update vegetation in the last 5 minutes
-              chrome.tabs.sendMessage(tabId, {
-                action: 'updateVegetation',
-                percentage,
-              });
-            }
-          }
-        }
-      });
-    });
-  }
+          if (tab && tab.url) {
+              const site = sites.find((s) => tab.url.includes(s.url));
+              if (site) {
+                  const totalTime = site.timer * 60;
+                  const remainingTime = totalTime - elapsedTime;
+                  const percentage = Math.max(0, (elapsedTime / totalTime) * 100);
 
+                  if (remainingTime <= 0) {
+                      sendMessageToTab(tabId, { action: "lockSite" });
+                  } else if (remainingTime <= 5 * 60) {
+                      sendMessageToTab(tabId, {
+                          action: "updateVegetation",
+                          percentage,
+                      });
+                  }
+              }
+          }
+      });
+  });
+}
