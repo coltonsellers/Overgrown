53d1469ad0fa653f57e175238b8a8ed762eeb3e9
coltonsellers <coltonsellers20@outlook.com>
2025-02-16 00:23:36 -0600

    Remove old functions (commented out)

--- src/components/WebsiteList.jsx
+++ src/components/WebsiteList.jsx
@@ -134,22 +134,3 @@ export default function WebsiteList() {
     </section>
   );
 }
-
-
-/*
-export default function WebsiteList({ websites }) {
-    return (
-        <div>
-            {websites.length === 0 ? (
-                <p className="text-gray-500">No websites being tracked.</p>
-            ) : (
-                <ul>
-                    {websites.map((website, index) => (
-                        <WebsiteItem key={index} title={website.title} timeLeft={website.timeLeft} />
-                    ))}
-                </ul>
-            )}
-        </div>
-    );
-};
-*/
