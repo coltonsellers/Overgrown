2673e02415b2d8758540990c9cd01fd70456773e
coltonsellers <coltonsellers20@outlook.com>
2025-02-15 21:48:50 -0600

    Add modal for adding new websites

--- src/components/WebsiteList.jsx
+++ src/components/WebsiteList.jsx
@@ -5,28 +5,42 @@ import {
   MDBCardBody,
   MDBCol,
   MDBContainer,
-  MDBInput,
   MDBRow,
   MDBTable,
   MDBTableBody,
   MDBTableHead,
+  MDBModal,
+  MDBModalBody,
+  MDBModalHeader,
+  MDBModalFooter,
+  MDBInput,
 } from "mdb-react-ui-kit";
 import WebsiteItem from "./WebsiteItem";
 
 export default function WebsiteList() {
-  // Sample state for websites
   const [websites, setWebsites] = useState([
     { title: "Buy groceries for next week", timeLeft: "In progress" },
     { title: "Renew car insurance", timeLeft: "In progress" },
     { title: "Sign up for online course", timeLeft: "In progress" },
   ]);
 
-  // Function to handle delete
+  const [newWebsite, setNewWebsite] = useState(""); // Stores new website name
+  const [modalOpen, setModalOpen] = useState(false); // Controls modal visibility
+
+  // Function to handle adding a new website
+  const handleAddWebsite = () => {
+    if (newWebsite.trim() === "") return;
+    setWebsites([...websites, { title: newWebsite, timeLeft: "In progress" }]);
+    setNewWebsite(""); // Clear input
+    setModalOpen(false); // Close modal
+  };
+
+  // Function to delete a website
   const handleDelete = (index) => {
     setWebsites(websites.filter((_, i) => i !== index));
   };
 
-  // Function to mark a task as finished
+  // Function to mark a website as finished
   const handleFinish = (index) => {
     setWebsites(
       websites.map((website, i) =>
@@ -42,20 +56,38 @@ export default function WebsiteList() {
           <MDBCol lg="9" xl="7">
             <MDBCard className="rounded-3">
               <MDBCardBody className="p-4">
-                <h4 className="text-center my-3 pb-3">To Do App</h4>
-                <MDBRow className="row-cols-lg-auto g-3 justify-content-center align-items-center mb-4 pb-2">
-                  <MDBCol size="12">
-                    <MDBInput label="Enter a task here" id="form1" type="text" />
-                  </MDBCol>
-                  <MDBCol size="12">
-                    <MDBBtn type="submit">Save</MDBBtn>
-                  </MDBCol>
-                  <MDBCol size="12">
-                    <MDBBtn type="submit" color="warning">
-                      Get tasks
+                <h4 className="text-center my-3 pb-3">Minimize the amount of time you spend on certain sites! Improve your </h4>
+
+                {/* Modal for Adding a New Website */}
+                <MDBModal show={modalOpen} setShow={setModalOpen}>
+                  <MDBModalHeader>Add New Website</MDBModalHeader>
+                  <MDBModalBody>
+                    <MDBInput
+                    label="Enter a website here"
+                    value={newWebsite}
+                    onChange={(e) => setNewWebsite(e.target.value)}
+                    />
+                  </MDBModalBody>
+                  <MDBModalFooter>
+                    <MDBBtn color="secondary" onClick={() => setModalOpen(false)}>
+                      Cancel
+                    </MDBBtn>
+                    <MDBBtn color="success" onClick={handleAddWebsite}>
+                      Save
+                    </MDBBtn>
+                  </MDBModalFooter>
+                </MDBModal>
+
+                {/* Buttons to Add New Wesbite */}
+                <MDBRow className="d-flex justify-content-center mb-4 pb-2">
+                  <MDBCol size="auto">
+                    <MDBBtn onClick={() => setModalOpen(true)} color="primary">
+                      Add
                     </MDBBtn>
                   </MDBCol>
                 </MDBRow>
+
+                {/* Table of Websites */}
                 <MDBTable className="mb-4">
                   <MDBTableHead>
                     <tr>
@@ -92,6 +124,7 @@ export default function WebsiteList() {
     </section>
   );
 }
+
 
 
 /*
